66b9fe928a807fabaf18cc51b04d7235c44165b3
Umut Keskin <ukeskin2626@gmail.com>
2021-10-24 12:09:16 +0300

    Add Search in Homepage 🔎

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-
+import React, { useState } from "react";
 export async function getStaticProps() {
   const res = await fetch(
     "https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations"
@@ -14,21 +14,47 @@ export async function getStaticProps() {
 }
 
 function Civilization({ data }) {
-  const civilizations = data.civilizations;
+  const [civilizations, setCivilizations] = useState(data.civilizations);
+
+  const handleSearch = (e) => {
+    const search = e.target.value;
+    const filtered = data.civilizations.filter((civilization) => {
+      return civilization.name.toLowerCase().includes(search.toLowerCase());
+    });
+    setCivilizations(filtered);
+  };
   return (
-    <ul>
-      {civilizations &&
-        civilizations.map((civilization) => (
-          <li key={civilization.id}>
+    <div className="min-w-full min-h-screen bg-blue-200">
+      <div className="container mx-auto py-8">
+        <h1 className="text-4xl font-sans font-bold text-center text-gray-800">
+          Age Of Empries Civilizations
+        </h1>
+        <div>
+          <input
+            onChange={handleSearch}
+            className="text-blue-900 my-4 text-xl w-full h-16 text-center shadow-xl"
+            type="text"
+            placeholder="Search here.."
+          />
+        </div>
+        <div className="bg-gray-800 text-white mt-4">
+          {civilizations.map((civilization) => (
             <Link
+              key={civilization.id}
               href="/civilization/[id]"
               as={`/civilization/${civilization.id}`}
             >
-              <a>{civilization.name}</a>
+              <a
+                className="flex flex-col w-full items-center text-2xl p-4 border-b-2 border-gray-600 hover:bg-gray-900"
+                href=""
+              >
+                {civilization.name}
+              </a>
             </Link>
-          </li>
-        ))}
-    </ul>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
